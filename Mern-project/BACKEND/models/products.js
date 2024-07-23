65dfc74f3d52c4217ba0484daa3d68370586603a
Moon ijaz <moonijaz006@gmail.com>
2024-07-23 18:45:16 +0500

    Add schema

--- Mern-project/BACKEND/models/products.js
+++ Mern-project/BACKEND/models/products.js
@@ -2,12 +2,60 @@ const mongoose = require('mongoose'); // Importing Mongoose for MongoDB interact
 
 // Defining a schema for the Product model
 const productSchema = mongoose.Schema({
-    name: String, // Name of the product
-    image: String, // URL of the product image
+    name: {
+        type: String,
+        required: true,
+    }, 
+    description:{
+        type: String,
+        required: true,
+    },
+    richDescription:{
+        type: String,
+        default: ""
+    },
+    image: {
+        type: String,
+        default: ""
+    }, 
+    images: {
+        type: String
+    }, 
+    brand: {
+        type: String,
+        default: ""
+    },     
+    price: {
+        type: Number,
+        default: 0
+    },     
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },     
     countInStock: {
-        type: Number, // Number of items in stock
-        required: true // Making this field required
-    }
+        type: Number, 
+        required: true,
+        min: 0,
+        max: 255 
+    },
+    rating: {
+        type: Number,
+        default: 0
+    }, 
+    numReviews: {
+        type: Number,
+        default: 0
+    }, 
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    }, 
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    }, 
 });
 
 // Creating a Mongoose model based on the schema
