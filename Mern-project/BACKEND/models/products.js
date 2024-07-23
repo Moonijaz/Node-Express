0846b30744d8bee8728ce1b7e75ff65a3ace774b
Moon ijaz <moonijaz006@gmail.com>
2024-07-23 18:16:15 +0500

    Add comments to file

--- Mern-project/BACKEND/models/products.js
+++ Mern-project/BACKEND/models/products.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
- 
+const mongoose = require('mongoose'); // Importing Mongoose for MongoDB interaction
+
+// Defining a schema for the Product model
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
+    name: String, // Name of the product
+    image: String, // URL of the product image
     countInStock: {
-        type: Number,
-        required: true
+        type: Number, // Number of items in stock
+        required: true // Making this field required
     }
 });
 
-exports.Product = mongoose.model('Product', productSchema);
+// Creating a Mongoose model based on the schema
+const Product = mongoose.model('Product', productSchema);
+
+// Exporting the Product model to use in other parts of the application
+module.exports = Product;
