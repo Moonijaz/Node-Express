e47e12ede839a2ec3c638d5246eaa49a2457d2d7
Moon ijaz <moonijaz006@gmail.com>
2024-07-24 18:16:34 +0500

    Add comments to file

--- Mern-project/BACKEND/app.js
+++ Mern-project/BACKEND/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 
 require("dotenv/config");
 const api = process.env.API_URL;
