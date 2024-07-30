566ad075f4d7ba371f64f9f4b343feb67f8050eb
Moon ijaz <moonijaz006@gmail.com>
2024-07-30 17:12:06 +0500

    Add post router to project.js

--- Mern-project/BACKEND/routes/products.js
+++ Mern-project/BACKEND/routes/products.js
@@ -1,29 +1,34 @@
 const express = require('express');
 const router =  express.Router();
 const Product = require('../models/products');
+const Category = require('../models/category');
 
 router.get('/', async (req, res) => {
     const productList = await Product.find();
     res.send(productList);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(500).send('Invalid Category');
+
     const product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     });
-
-    product .save()
-        .then((createdProduct) => {
-            res.status(201).json(createdProduct);
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err,
-                success: false
-            });
-        });
+    const savedProduct = await product.save();
+    if(!savedProduct)
+        return res.status(500).send('The product cannot be created')
+    res.send(savedProduct);
 });
 
 module.exports = router;
